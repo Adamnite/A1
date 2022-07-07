1d7cef03cd43787f8f786c96a07fe8c213cee65e
mike2550 <106352483+mike2550@users.noreply.github.com>
2022-07-06 22:19:49 -0400

    Add files via upload

---
+++ liba1/A1_Compiler/source/expression.hpp
@@ -0,0 +1,41 @@
+// Copyright 2022 The Adamnite C++ Authors
+//This code is open-sourced under the MIT License.
+
+#ifndef expression_hpp
+#define expression_hpp
+
+#include "variable.hpp"
+#include "types.hpp"
+
+#include <string>
+
+namespace stork {
+	class runtime_context;
+	class tokens_iterator;
+	class compiler_context;
+
+	template <typename R>
+	class expression {
+		expression(const expression&) = delete;
+		void operator=(const expression&) = delete;
+	protected:
+		expression() = default;
+	public:
+		using ptr = std::unique_ptr<const expression>;
+		
+		virtual R evaluate(runtime_context& context) const = 0;
+		virtual ~expression() = default;
+	};
+	
+	expression<void>::ptr build_void_expression(compiler_context& context, tokens_iterator& it);
+	expression<number>::ptr build_number_expression(compiler_context& context, tokens_iterator& it);
+	expression<lvalue>::ptr build_initialization_expression(
+		compiler_context& context,
+		tokens_iterator& it,
+		type_handle type_id,
+		bool allow_comma
+	);
+	expression<lvalue>::ptr build_default_initialization(type_handle type_id);
+}
+
+#endif /* expression_hpp */
---
+++ liba1/A1_Compiler/source/main.cpp
@@ -0,0 +1,29 @@
+// Copyright 2022 The Adamnite C++ Authors
+//This code is open-sourced under the MIT License.
+
+#include <iostream>
+#include "module.hpp"
+#include "standard_functions.hpp"
+
+int main() {
+	std::string path = __FILE__;
+	path = path.substr(0, path.find_last_of("/\\") + 1) + "test.A1";
+	
+	using namespace stork;
+	
+	module m;
+	
+	add_standard_functions(m);
+	
+	m.add_external_function("greater", std::function<number(number, number)>([](number x, number y){
+		return x > y;
+	}));
+	
+	auto s_main = m.create_public_function_caller<void>("main");
+	
+	if (m.try_load(path.c_str(), &std::cerr)) {
+		s_main();
+	}
+	
+	return 0;
+}
