47c0a15093e954ed6f02776e11b97c27e275c871
Arch222 <66220753+Arch222@users.noreply.github.com>
2022-08-22 17:19:13 -0400

    Update common.hpp

--- CoreUtils/Include/CoreUtils/common.hpp
+++ CoreUtils/Include/CoreUtils/common.hpp
@@ -3,8 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_CRYPTO_COMMON_H
-#define BITCOIN_CRYPTO_COMMON_H
+#pragma once
 
 
 #include <string>
