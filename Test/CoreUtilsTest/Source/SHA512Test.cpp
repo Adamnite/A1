c49024817778fdaa6b7ba739c2c3ef2179310b1e
m-peko <marinpeko5@gmail.com>
2022-08-26 09:21:06 +0200

    Refactor Sha512

--- Test/CoreUtilsTest/Source/SHA512Test.cpp
+++ Test/CoreUtilsTest/Source/SHA512Test.cpp
@@ -6,25 +6,61 @@
  */
 
 
-#include "CoreUtils/Sha512.hpp"
-// #include "TestUtils.hpp"
+#include <CoreUtils/Sha512.hpp>
 
 #include <gtest/gtest.h>
-SHA512 sha512 = SHA512();
-TEST(SHA512Test, testAgainstLargeKnownValues)
-{
-    // https://emn178.github.io/online-tools/sha512.html
-    // this sites used to get known goods
-    EXPECT_EQ(sha512.hash("abckjndnsfgljadsfhgknskjdlah;sda gh ;sdhagjk leg bjhklafdhgl lalkjsgligera"), "08d24a15fc1bc9d0e79381942b085d7288252ad735815e888619be0cfaf0cf2936ad3286d8dc5da936f0a594116f07a4296d569682d3dc807af4f2dc7ca6cbe6");
-}
-TEST(SHA512Test, againstShortKnownValues){
-    // https://emn178.github.io/online-tools/sha512.html
-    // this sites used to get known goods
 
-    // EXPECT_EQ(sha512.hash("A123456789101112"),"a2e2dfddc0dc16cb395ed13736efd351fe601a2097510238dc6a1a4b41d2c21f6ce049e03767637bb88f89aa1603778c493246b2746765a64ad8a5dfb1d54106");
-    // EXPECT_EQ(sha512.hash("A12345678910111213"),"efe76722f2111344b98fbb3b7bd5d82dfc35aa4aa4f8d8a2e21fe22ff70dae330b0774ff17f4af33ed6527091d490ac40daf8cce3d1ab12b250e9971b1d97c38");
-    // EXPECT_EQ(sha512.hash("A1234567891011121314"),"efe76722f2111344b98fbb3b7bd5d82dfc35aa4aa4f8d8a2e21fe22ff70dae330b0774ff17f4af33ed6527091d490ac40daf8cce3d1ab12b250e9971b1d97c38");
-    // EXPECT_EQ(sha512.hash("A123456789101112131415"),"efe76722f2111344b98fbb3b7bd5d82dfc35aa4aa4f8d8a2e21fe22ff70dae330b0774ff17f4af33ed6527091d490ac40daf8cce3d1ab12b250e9971b1d97c38");
-    // EXPECT_EQ(sha512.hash("abc"),"ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f");
-    // EXPECT_EQ(sha512.hash(""), "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e");
+TEST( Sha512Test, hash )
+{
+    using namespace A1::Utils;
+
+    EXPECT_EQ
+    (
+        Sha512::hash( "" ),
+        "cf83e1357eefb8bdf1542850d66d8007d620e4050b5"
+        "715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318"
+        "d2877eec2f63b931bd47417a81a538327af927da3e"
+    );
+
+    EXPECT_EQ
+    (
+        Sha512::hash( "test" ),
+        "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772"
+        "e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc"
+        "887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"
+    );
+
+    EXPECT_EQ
+    (
+        Sha512::hash( "testtesttest" ),
+        "623c92d8c3e80a6963599e42aa37d43f8f4f4e84c74"
+        "2bfe5cf26b33b6e5a281599dd9e948691b5f76566e5"
+        "26375ef46cc5485af55bac2a198b69b40333ac92fb"
+    );
+
+    EXPECT_EQ
+    (
+        Sha512::hash
+        (
+            "testtesttesttesttesttesttesttesttesttest"
+            "testtesttesttesttesttesttesttesttesttest"
+        ),
+        "4785d36bea718ced58e2d3e2c3c1340725b8d5bc9c0"
+        "61abba6a47e3eade1e7701fe0ce2b3f2c640f58f6b9"
+        "7ccb85f2b00c31b42a84112127d750c5e0e80b6d37"
+    );
+
+    // EXPECT_EQ
+    // (
+    //     Sha512::hash
+    //     (
+    //         "testtesttesttesttesttesttesttesttesttest"
+    //         "testtesttesttesttesttesttesttesttesttest"
+    //         "testtesttesttesttesttesttesttesttesttest"
+    //         "testtesttesttesttesttesttesttesttesttest"
+    //     ),
+    //     "039f74360411cd1c028f1f761e2ced9e543457d8009"
+    //     "0e0dca2579405805451312d09b5a77de4a2bb6b86a7"
+    //     "31d20468fa48500c36e5a757c52e830bf2f5434998"
+    // );
 }
