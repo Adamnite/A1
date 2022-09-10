aa0638aef287a78b52b243fa3013f4fef7335168
m-peko <marinpeko5@gmail.com>
2022-09-10 12:53:43 +0200

    Add new ENABLE_LLVM option, link CoreLib to LLVM

--- CoreLib/CoreLib.cmake
+++ CoreLib/CoreLib.cmake
@@ -1,5 +1,8 @@
 include_guard()
 
+option( ENABLE_LLVM "Enable LLVM" ON )
+message( STATUS "LLVM enabled: ${ENABLE_LLVM}" )
+
 include( ${CMAKE_CURRENT_LIST_DIR}/CoreLib.srcs.cmake )
 
 add_library( CoreLib STATIC ${SOURCES} )
@@ -9,3 +12,24 @@ target_include_directories(
     PUBLIC  ${CMAKE_CURRENT_LIST_DIR}/Include
     PRIVATE ${CMAKE_CURRENT_LIST_DIR}/Source
 )
+
+if( ENABLE_LLVM )
+    # Prerequisites:
+    # - install Clang / LLVM using Homebrew
+    #       $ brew install llvm
+    #       $ export PATH="$(brew --prefix llvm)/bin:${PATH}"
+
+    find_package( LLVM REQUIRED CONFIG )
+    message( STATUS "LLVM version: ${LLVM_PACKAGE_VERSION}" )
+
+    target_include_directories( CoreLib AFTER PRIVATE ${LLVM_INCLUDE_DIRS} )
+    add_definitions( ${LLVM_DEFINITIONS} )
+
+    llvm_map_components_to_libnames( LLVMLibs
+        core
+        irreader
+        support
+    )
+
+    target_link_libraries( CoreLib PRIVATE ${LLVMLibs} )
+endif()
