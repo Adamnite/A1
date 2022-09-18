f64ea46d4aa9dd22f66c826a02ada597967d0748
m-peko <marinpeko5@gmail.com>
2022-09-18 10:28:31 +0200

    Check for identifiers scope

--- CoreLib/Source/Compiler/LLVM/Codegen.cpp
+++ CoreLib/Source/Compiler/LLVM/Codegen.cpp
@@ -39,6 +39,17 @@ namespace A1::LLVM
 
 namespace
 {
+    using ScopeIdentifiers = std::map< std::string, llvm::Value * >;
+
+    llvm::Value * getFromScope( ScopeIdentifiers const & scope, std::string const & name ) noexcept
+    {
+        if ( auto it{ scope.find( name ) }; it != std::end( scope ) )
+        {
+            return it->second;
+        }
+        return nullptr;
+    }
+
     std::unique_ptr< llvm::LLVMContext > context;
     std::unique_ptr< llvm::IRBuilder<> > builder;
     std::unique_ptr< llvm::Module      > module_;
@@ -51,7 +62,8 @@ namespace
     (
         IRBuilderUnaryClbk< T ... >      const,
         std::span< Node::Pointer const > const,
-        std::string_view                 const
+        std::string_view                 const,
+        ScopeIdentifiers                 const &
     );
 
     template< typename ... T >
@@ -62,12 +74,13 @@ namespace
     (
         IRBuilderBinaryClbk< T ... >     const,
         std::span< Node::Pointer const > const,
-        std::string_view                 const
+        std::string_view                 const,
+        ScopeIdentifiers                 const &
     );
 
     llvm::Function * codegenFunctionDefinition( std::span< Node::Pointer const > const );
 
-    llvm::Value * codegenImpl( Node::Pointer const & node )
+    llvm::Value * codegenImpl( Node::Pointer const & node, ScopeIdentifiers const & scope )
     {
         if ( node == nullptr )
         {
@@ -82,47 +95,47 @@ namespace
                 {
                     switch ( type ){
                         case NodeType::Multiplication:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateFMul, node->children(), "multmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateFMul, node->children(), "multmp", scope );
                         case NodeType::Division:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateFDiv, node->children(), "divtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateFDiv, node->children(), "divtmp", scope );
                         case NodeType::FloorDivision:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateSDiv, node->children(), "fdivtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateSDiv, node->children(), "fdivtmp", scope );
                         case NodeType::Modulus:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateFRem, node->children(), "modtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateFRem, node->children(), "modtmp", scope );
                         case NodeType::Addition:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateFAdd, node->children(), "addtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateFAdd, node->children(), "addtmp", scope );
                         case NodeType::Subtraction:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateFSub, node->children(), "subtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateFSub, node->children(), "subtmp", scope );
                         case NodeType::BitwiseLeftShift:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateShl, node->children(), "lshtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateShl, node->children(), "lshtmp", scope );
                         case NodeType::BitwiseRightShift:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateAShr, node->children(), "rshtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateAShr, node->children(), "rshtmp", scope );
                         case NodeType::BitwiseAnd:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateAnd, node->children(), "andtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateAnd, node->children(), "andtmp", scope );
                         case NodeType::BitwiseOr:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateOr, node->children(), "ortmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateOr, node->children(), "ortmp", scope );
                         case NodeType::BitwiseXor:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateXor, node->children(), "xortmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateXor, node->children(), "xortmp", scope );
                         case NodeType::BitwiseNot:
-                            return codegenUnary( &llvm::IRBuilder<>::CreateNot, node->children(), "nottmp" );
+                            return codegenUnary( &llvm::IRBuilder<>::CreateNot, node->children(), "nottmp", scope );
                         case NodeType::Equality:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateFCmpOEQ, node->children(), "eqtmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateFCmpOEQ, node->children(), "eqtmp", scope );
                         case NodeType::Inequality:
-                            return codegenBinary( &llvm::IRBuilder<>::CreateFCmpONE, node->children(), "netmp" );
+                            return codegenBinary( &llvm::IRBuilder<>::CreateFCmpONE, node->children(), "netmp", scope );
                         case NodeType::FunctionDefinition:
                             return codegenFunctionDefinition( node->children() );
                         case NodeType::StatementReturn:
                         {
                             ASSERT( std::size( node->children() ) == 1U );
-                            return codegenImpl( node->children()[ 0 ] );
+                            return codegenImpl( node->children()[ 0 ], scope );
                         }
                         default:
                             return nullptr;
                     }
                 },
-                []( Identifier const & ) -> llvm::Value *
+                [ & ]( Identifier const & identifier ) -> llvm::Value *
                 {
-                    return nullptr;
+                    return getFromScope( scope, identifier.name );
                 },
                 []( Number const number ) -> llvm::Value *
                 {
@@ -144,18 +157,16 @@ namespace
     template< typename ... T >
     llvm::Value * codegenUnary
     (
-        IRBuilderUnaryClbk< T ... >      const clbk,
-        std::span< Node::Pointer const > const nodes,
-        std::string_view                 const name
+        IRBuilderUnaryClbk< T ... >      const   clbk,
+        std::span< Node::Pointer const > const   nodes,
+        std::string_view                 const   name,
+        ScopeIdentifiers                 const & scope
     )
     {
         ASSERT( std::size( nodes ) == 1U );
-        auto * lhs{ codegenImpl( nodes[ 0U ] ) };
+        auto * lhs{ codegenImpl( nodes[ 0U ], scope ) };
 
-        if ( lhs == nullptr )
-        {
-            return nullptr;
-        }
+        if ( lhs == nullptr ) { return nullptr; }
 
         return ( *builder.*clbk )( lhs, name, T{} ... );
     }
@@ -163,25 +174,25 @@ namespace
     template< typename ... T >
     llvm::Value * codegenBinary
     (
-        IRBuilderBinaryClbk< T ... >     const clbk,
-        std::span< Node::Pointer const > const nodes,
-        std::string_view                 const name
+        IRBuilderBinaryClbk< T ... >     const   clbk,
+        std::span< Node::Pointer const > const   nodes,
+        std::string_view                 const   name,
+        ScopeIdentifiers                 const & scope
     )
     {
         ASSERT( std::size( nodes ) == 2U );
-        auto * lhs{ codegenImpl( nodes[ 0U ] ) };
-        auto * rhs{ codegenImpl( nodes[ 1U ] ) };
+        auto * lhs{ codegenImpl( nodes[ 0U ], scope ) };
+        auto * rhs{ codegenImpl( nodes[ 1U ], scope ) };
 
-        if ( lhs == nullptr || rhs == nullptr )
-        {
-            return nullptr;
-        }
+        if ( lhs == nullptr || rhs == nullptr ) { return nullptr; }
 
         return ( *builder.*clbk )( lhs, rhs, name, T{} ... );
     }
 
     llvm::Function * codegenFunctionDefinition( std::span< Node::Pointer const > const children )
     {
+        ASSERT( std::size( children ) >= 2U );
+
         auto functionBodyStartIdx{ 0 };
 
         std::vector< std::string > paramNames;
@@ -232,16 +243,15 @@ namespace
         auto * block{ llvm::BasicBlock::Create( *context, "entry", function ) };
         builder->SetInsertPoint( block );
 
-        std::map< std::string, llvm::Value * > identifiersInScope;
+        ScopeIdentifiers functionScope;
 
         // record the function arguments
         for ( auto & arg : function->args() )
         {
-            identifiersInScope[ std::string{ arg.getName() }] = &arg;
+            functionScope[ std::string{ arg.getName() }] = &arg;
         }
 
-        // TODO: pass identifiers in scope to the body codegen
-        if ( auto * return_{ codegenImpl( children[ functionBodyStartIdx ] ) } )
+        if ( auto * return_{ codegenImpl( children[ functionBodyStartIdx ], functionScope ) } )
         {
             builder->CreateRet( return_ );
 
@@ -268,13 +278,13 @@ std::unique_ptr< Module > codegen
     builder = std::make_unique< llvm::IRBuilder<> >( *context );
     module_ = std::make_unique< llvm::Module >( "Module", *context );
 
-    /**
-     * Optimizations benefit from knowing about the target and data layout.
-     */
+    // optimizations benefit from knowing about the target and data layout
     module_->setDataLayout  ( dataLayout   );
     module_->setTargetTriple( targetTriple );
 
-    codegenImpl( node );
+    ScopeIdentifiers moduleScope;
+
+    codegenImpl( node, moduleScope );
 
     return std::move( module_ );
 }
