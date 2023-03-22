ee3e110bb7becb19903a209f30509c949e2947be
m-peko <marinpeko5@gmail.com>
2023-03-22 23:12:59 +0100

    Update comment in cmake

--- CMake/Common.cmake
+++ CMake/Common.cmake
@@ -2,7 +2,8 @@ include_guard()
 
 macro( enable_sanitizers target )
     if( CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
-        # -fsanitize=undefined includes vptr sanitizer which requires RTTI to be enabled
+        # -fsanitize=undefined includes vptr sanitizer which requires RTTI to be enabled.
+        # Therefore, disable vptr sanitizer as LLVM is built with RTTI disabled.
         target_compile_options( ${target} PRIVATE -fsanitize=undefined -fsanitize=address -fno-sanitize=vptr )
         target_link_libraries ( ${target} PUBLIC  -fsanitize=undefined -fsanitize=address -fno-sanitize=vptr )
     endif()
